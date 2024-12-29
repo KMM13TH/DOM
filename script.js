70b9c2943e176690f2eeaf1efb96ce38f2e4324a
sandhyakamath <sandhya_kammath@yahoo.co.in>
2024-12-29 18:55:44 +0000

    DOM activity completed

---
+++ script.js
@@ -0,0 +1,9 @@
+var heading = document.getElementById("myheading").innerHTML;
+document.write(heading);
+
+function getSurprise() {
+    document.getElementById("surprise").innerHTML = "😁 You are Awesome!";
+    document.getElementById("surprise").style.color = "seagreen";
+}
+
+document.body.style.backgroundColor = "azure";
